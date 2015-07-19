ac532027004de95dc7a6230c915075728456912d
Tatiana Baklan <baklantat@gmail.com>
2015-07-19 15:32:14 +0300

    Add GET, POST

--- nodeServer.js
+++ nodeServer.js
@@ -1,10 +1,17 @@
 var http = require('http');
 var urlparse = require('url');
+var qs = require('querystring');
 var students = require('./data.js');
 
 var server = http.createServer(function (req, res) {
+	var body; //request body
+
+	req.on('data', function(chunk){
+		body += chunk.toString();
+	})
 
 	var url = urlparse.parse(req.url, true);
+	var query = url.parse(req.url,true).query;
 	
 	//the full path
 	var urlpathname = url.pathname;
@@ -14,88 +21,124 @@ var server = http.createServer(function (req, res) {
 	var studentsPath = argArray[1];
 	var idPath = argArray[2];
 	var otherPath = argArray[3];
+	
 
 	switch(req.method) {
 
 		
 		case "GET": 
-			
-			if (studentsPath == "students" && idPath != "") {
 
-				if (otherPath == "" || otherPath == undefined) {
-					res.end ("GET/students/id ");
-					return;
-				}	
-			}
+		if (studentsPath == "students" && idPath != "" && idPath != undefined) {
 
-			if (studentsPath == "students") {
-				
-				if (idPath == "" || idPath == undefined) {
-					res.write( JSON.stringify(students.getAll()));
+			if (otherPath == "" || otherPath == undefined) {
+
+				var respond = students.getById(idPath);
+
+				if (respond === null) {
+					res.write(JSON.stringify(students.error()));
 					res.end();
 					return;
-				}	
+				}
 
-			}
-			
-			if (studentsPath == "") {
-				res.end("Node sever is running");
+				res.write(JSON.stringify(respond));
+				res.end()
 				return;
-			}
-			
-			res.write( JSON.stringify(students.error()));
-			res.end();
+			}	
+		}
+
+		if (studentsPath == "students") {
+
+			if (idPath == "" || idPath == undefined) {
+				res.write( JSON.stringify(students.getAll()));
+				res.end();
+				return;
+			}	
+
+		}
+
+		if (studentsPath == "") {
+			res.end("Node sever is running");
 			return;
-			
-			break;
+		}
+
+		res.write( JSON.stringify(students.error()));
+		res.end();
+		return;
+
+		break;
 		
 
 		
 		case "POST": 
+
+		req.on('end', function(chunk){
+			//remove wierd "undefined"
+			var requestStr = body.replace("undefined", "");
+			var request = JSON.parse(requestStr);
 			
 			if (studentsPath == "students") {
 				
 				if (idPath == "" || idPath == undefined) {
-					res.end ("POST/ works");
+					
+					var student = request.name;
+					var grade = request.totalGrade; 
+
+					if (student == undefined || grade == undefined) {
+						res.write(JSON.stringify(students.error()));
+						res.end();
+						return;
+					}
+
+					students.add(student, grade);
+		//			res.end(JSON.stringify(students.success()));
+					res.end( JSON.stringify(students.getAll()));
 					return;
+
 				}	
 			}
 			res.write( JSON.stringify(students.error()));
 			res.end();
 			return;
+		})
 
-			break;
+		break;
 
-		
+
 		
 		case "PUT": 
 
-			if (studentsPath == "students" || idPath != "") {
-					
-					res.end ("PUT/ works");
-					return;
-					
-			}
-			res.write( JSON.stringify(students.error()));
-			res.end();
-			return;
+		if (studentsPath == "students" && idPath != "" &&  idPath != undefined) {
 
-			break;
+			if (otherPath == "" || otherPath == undefined) {
+				res.end ("PUT/ works");
+				return;
+			}	
+
+		}
+		res.write( JSON.stringify(students.error()));
+		res.end();
+		return;
+
+		break;
 		
+		
+
 		case "DELETE":
 
-				if (studentsPath == "students" || idPath != "") {
-				
-					res.end ("DELETE/ works");
-					return;	
-			}
-			res.write( JSON.stringify(students.error()));
-			res.end();
-			return;
+		if (studentsPath == "students" && idPath != "" &&  idPath != undefined) {
 
-			break;
+			if (otherPath == "" || otherPath == undefined) {
+				res.end ("DELETE/ works");
+				return;
+			}	
 
-			
+		}
+
+		res.write( JSON.stringify(students.error()));
+		res.end();
+		return;
+
+		break;		
 	}
 
 }).listen(8080, '127.0.0.1');
